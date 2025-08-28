e75c3897a68186865bfc4e7da0c557d6e31dac3d
Guillaume Verger <verger.guillaume@gmail.com>
2025-08-28 23:51:16 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Raylib development environment";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs, ... }:
+    let system = "x86_64-linux";
+    in {
+      devShells."${system}".default =
+        let pkgs = import nixpkgs { inherit system; };
+        in pkgs.mkShell {
+          packages = [
+            pkgs.libGL
+
+            # X11 dependencies
+            pkgs.xorg.libX11
+            pkgs.xorg.libX11.dev
+            pkgs.xorg.libXcursor
+            pkgs.xorg.libXi
+            pkgs.xorg.libXinerama
+            pkgs.xorg.libXrandr
+
+            pkgs.just
+            pkgs.raylib
+            # pkgs.emscripten # websupport for raylib
+          ];
+
+          # Audio dependencies
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.alsa-lib ];
+
+          inputsFrom = [
+            pkgs.raylib
+          ];
+        };
+    };
+}
